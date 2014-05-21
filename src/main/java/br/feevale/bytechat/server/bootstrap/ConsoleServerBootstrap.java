e9f3b8de1b9bfc777e838e0d41a9f75a1c62b39c
gabrielhof <gabrielhof@gmail.com>
2014-05-21 09:44:03 -0300

    Buscar a porta nas variaveis de ambiente

--- src/main/java/br/feevale/bytechat/server/bootstrap/ConsoleServerBootstrap.java
+++ src/main/java/br/feevale/bytechat/server/bootstrap/ConsoleServerBootstrap.java
@@ -1,5 +1,7 @@
 package br.feevale.bytechat.server.bootstrap;
 
+import org.apache.commons.lang.StringUtils;
+
 import br.feevale.bytechat.config.Configuration;
 import br.feevale.bytechat.exception.ServerException;
 import br.feevale.bytechat.server.ChatServer;
@@ -8,8 +10,10 @@ import br.feevale.bytechat.server.SimpleChatServer;
 public class ConsoleServerBootstrap {
 
 	public static void main(String[] args) throws ServerException {
+		String port = System.getenv("PORT");
+		
 		Configuration config = new Configuration();
-		config.setPort(8080);
+		config.setPort(StringUtils.isBlank(port) ? 8080 : Integer.parseInt(port));
 		
 		ChatServer server = new SimpleChatServer(config);
 		server.start();
