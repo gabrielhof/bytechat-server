aa3b8745f5ad02735f8cbaf49ddf4e4626ef8b76
Gabriel Hoff <gabriel@i360tecnologia.com.br>
2014-05-22 13:19:31 -0300

    Bombando nda VPS da DigitalOcean

--- src/main/java/br/feevale/bytechat/server/bootstrap/ConsoleServerBootstrap.java
+++ src/main/java/br/feevale/bytechat/server/bootstrap/ConsoleServerBootstrap.java
@@ -1,7 +1,5 @@
 package br.feevale.bytechat.server.bootstrap;
 
-import org.apache.commons.lang.StringUtils;
-
 import br.feevale.bytechat.config.Configuration;
 import br.feevale.bytechat.exception.ServerException;
 import br.feevale.bytechat.server.ChatServer;
@@ -10,10 +8,7 @@ import br.feevale.bytechat.server.SimpleChatServer;
 public class ConsoleServerBootstrap {
 
 	public static void main(String[] args) throws ServerException {
-		String port = System.getenv("PORT");
-		
-		Configuration config = new Configuration();
-		config.setPort(StringUtils.isBlank(port) ? 8080 : Integer.parseInt(port));
+		Configuration config = Configuration.fromProperties();
 		
 		ChatServer server = new SimpleChatServer(config);
 		server.start();
